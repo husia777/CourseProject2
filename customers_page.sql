443ce368d57772f3578e31328db5ba66d0f8377a
husia777 <naimovsarif411@gmail.com>
2022-10-19 14:24:32 +0500

    init5

---
+++ customers_page.sql
@@ -0,0 +1,10 @@
+SELECT COUNT(*) FROM  customers  --1команда
+
+SELECT  DISTINCT city ,  country FROM  customers    --2команда
+SELECT  orders.ship_name, employees.last_name, employees.first_name
+FROM  employees
+
+JOIN orders ON orders.employee_id = employees.employee_id
+JOIN shippers ON shippers.shipper_id = orders.ship_via
+WHERE orders.ship_city = 'London' AND employees.city = 'London' AND shippers.company_name = 'Speedy Express' --2команда!
+ -- 4 ЗАПРОС ПРОПУЩЕН
---
+++ orders_page.sql
@@ -0,0 +1,8 @@
+SELECT * FROM orders
+ORDER BY required_date DESC, shipped_date --1команда
+
+SELECT AVG(shipped_date - order_date) from orders
+WHERE ship_country = 'USA' --2команда
+
+SELECT product_name, unit_price * units_in_stock as sum_product FROM products
+where discontinued = 1 --3команда
