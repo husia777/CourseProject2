34edf3ae86b111e0677278e008aea878b6f47132
husia777 <naimovsarif411@gmail.com>
2022-10-19 12:28:19 +0500

    init4

--- main.py
+++ main.py
@@ -36,17 +36,13 @@ def insert_data_in_suppliers():
             product = [j.replace("'", "''") for j in i['products']]
             updated_data_for_suppliers = f"""UPDATE products SET suppliers_id = {i['id']} WHERE product_name in ('{"', '".join(product)}');\n """
             file.write(updated_data_for_suppliers)
-        file.write("""
-ALTER TABLE ONLY products
-        ALTER TABLE  products
-    ADD CONSTRAINT fk_product_id FOREIGN KEY (suppliers_id) REFERENCES suppliers(suppliers_id);\n""")
+
 
     with open('file.sql', 'a', encoding='utf-8') as file:
         for i in data:
             file.write(f"""INSERT INTO suppliers VALUES ({i['id']}, '{i['company_name'].replace("'", '')}', '{i['contact'].split(',')[0]}', '{i['contact'].split(',')[1]}','{i['address'].split(';')[0].strip(';')}','{i['address'].split(';')[1].strip(';')}','{i['address'].split(';')[2].strip(';')}','{i['address'].split(';')[3].strip(';')}','{i['address'].split(';')[4].replace("'", '')}', '{i['phone']}','{i['fax'] if len(i['fax'])>0 else 0}','{i['homepage'].replace("'", '') if len(i['homepage'])>0 else 0}');\n""")
-
-
-#Заполнить таблицу саплаерс
+    with open("file.sql", 'a', encoding='utf-8') as file:
+        file.write("""ALTER TABLE  products ADD CONSTRAINT fk_suppliers_id FOREIGN KEY(suppliers_id)  REFERENCES suppliers(suppliers_id);\n""")
 
 
 def main():
