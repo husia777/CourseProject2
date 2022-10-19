ae4953a4cb49cb6d324e1b48e57320ed1839900b
husia777 <naimovsarif411@gmail.com>
2022-10-19 18:25:23 +0500

    init7

--- customers_page.sql
+++ customers_page.sql
@@ -4,10 +4,10 @@ SELECT  DISTINCT city ,  country FROM  customers    --2команда
 SELECT  orders.ship_name, employees.last_name, employees.first_name
 FROM  employees;
 
+SELECT * FROM employees
 JOIN orders ON orders.employee_id = employees.employee_id
 JOIN shippers ON shippers.shipper_id = orders.ship_via
-WHERE orders.ship_city = 'London' AND employees.city = 'London' AND shippers.company_name = 'Speedy Express'; --2команда!
+WHERE orders.ship_city = 'London' AND employees.city = 'London' AND shippers.company_name = 'Speedy Express'; --3команда!
 
  -- 4 ЗАПРОС ПРОПУЩЕН
 
---страны из кастомера пересечение с саплаер потом экзепт стран в эмплоерс
